e99f1e6a946d0bab945620d870237deae4600534
SudhakarGVVN <sudhakar@zanjo.io>
2017-05-17 18:02:16 +0530

    Update 17/05/2017

--- app/src/main/java/solutions/it/zanjo/travease/Activities/AboutHotelActivity.java
+++ app/src/main/java/solutions/it/zanjo/travease/Activities/AboutHotelActivity.java
@@ -3,6 +3,8 @@ package solutions.it.zanjo.travease.Activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import solutions.it.zanjo.travease.R;
@@ -18,5 +20,12 @@ public class AboutHotelActivity extends AppCompatActivity {
 
         TextView title= (TextView) toolbar.findViewById(R.id.TitleTV);
         title.setText("About Hotel Granduce House");
+        ImageView backBT= (ImageView) toolbar.findViewById(R.id.backBT);
+        backBT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
